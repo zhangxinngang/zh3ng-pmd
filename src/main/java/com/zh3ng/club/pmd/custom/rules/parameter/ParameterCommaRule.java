a37331fc5fc5b07af0f007c14c3f6ef9477ba46e
zhangxingang <zhangxingang@maoyan.com>
2020-05-18 11:43:45 +0800

    收敛两个参数规则 到一个类中

--- src/main/java/com/zh3ng/club/pmd/custom/rules/parameter/ParameterCommaRule.java
+++ src/main/java/com/zh3ng/club/pmd/custom/rules/parameter/ParameterCommaRule.java
@@ -1,6 +1,7 @@
 package com.zh3ng.club.pmd.custom.rules.parameter;
 
 import net.sourceforge.pmd.RuleContext;
+import net.sourceforge.pmd.lang.ast.Node;
 import net.sourceforge.pmd.lang.java.ast.*;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
@@ -9,10 +10,16 @@ import java.util.List;
 /**
  * @author zhangxingang
  * @created on 2020-05-12
- *
- * 方法的定义 参数之间逗号后要加一个空格
+ * 关于方法 参数的代码规则
  */
 public class ParameterCommaRule extends AbstractJavaRule {
+
+    /**
+     * 方法的定义-参数之间逗号后要加一个空格
+     * @param node
+     * @param data
+     * @return
+     */
     @Override
     public Object visit(ASTMethodDeclaration node, Object data) {
         ASTMethodDeclarator declarator = node.getFirstChildOfType(ASTMethodDeclarator.class);
@@ -35,6 +42,34 @@ public class ParameterCommaRule extends AbstractJavaRule {
                 }
             }
         }
+        return super.visit(node, data);
+    }
+
+    /**
+     * 方法的调用-参数之间逗号分隔，逗号之后加一个空格
+     * @param node
+     * @param data
+     * @return
+     */
+    @Override
+    public Object visit(ASTPrimaryExpression node, Object data) {
+        int nodeCount = node.jjtGetNumChildren();
+        for (int i = 0;i<nodeCount;i++){
+            Node n = node.jjtGetChild(i);
+            if (n.getClass() == ASTPrimarySuffix.class && ((ASTPrimarySuffix)n).getArgumentCount() > 0){
+                ASTArguments arguments = ((ASTPrimarySuffix)n).getFirstChildOfType(ASTArguments.class);
+                ASTArgumentList argumentList = arguments.getFirstChildOfType(ASTArgumentList.class);
+                RuleContext ruleContext = (RuleContext)data;
+                for (int j=0;j < arguments.getArgumentCount() - 1;j++){
+                    if (argumentList.jjtGetChild(i+1).getBeginLine() == argumentList.jjtGetChild(i).getBeginLine()
+                            && argumentList.jjtGetChild(i+1).getBeginColumn() - argumentList.jjtGetChild(i).getEndColumn() != 3){
+                        String message = ruleContext.getSourceCodeFilename() +" "+ node.getBeginLine()+" 方法的调用-参数之间逗号分隔，逗号之后加一个空格";
+                        this.addViolation(data, node, message);
+                    }
+                }
+            }
+        }
+
         return super.visit(node, data);
     }
 
